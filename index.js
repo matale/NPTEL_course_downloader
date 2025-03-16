9e16b8eef20aceb6ac5949494540339372fc8537
Roman <roman.mbwasi@later.com>
2025-03-16 08:07:37 -0400

    Added transcripts download

--- index.js
+++ index.js
@@ -68,36 +68,133 @@ console.log("Config loaded successfully:", config);
 
 // Accessing config values
 const COURSE_IDS_TO_DOWNLOAD = config.course_ids_to_download;
+const DOWNLOAD_VIDEOS = config.download_videos;
 const TRANSCRIPT_LANGUAGES = config.transcript_languages;
 
-async function downloadVideo(video, course_title, course_id) {
-  const videosDir = `downloads/${course_id}_${course_title}/videos`.replace(
-    /[\s:]+/g,
-    "_"
+async function downloadVideo(videos, courseTitle, courseId) {
+  if (!DOWNLOAD_VIDEOS) {
+    console.warn("Configured not to download videos, skipping.");
+  }
+  if (!videos || !Array.isArray(videos)) {
+    throw new Error("Invalid course downloads data");
+  }
+
+  console.log(
+    `Total ${videos.length} video files to download for course ${courseTitle}.`
   );
 
-  if (!fs.existsSync(videosDir)) {
-    fs.mkdirSync(videosDir, { recursive: true });
+  while (videos.length > 0) {
+    const video = videos.shift();
+
+    const videosDir = `downloads/${courseId}_${courseTitle}/videos`.replace(
+      /[\s:]+/g,
+      "_"
+    );
+
+    if (!fs.existsSync(videosDir)) {
+      fs.mkdirSync(videosDir, { recursive: true });
+    }
+    const { title, url, lesson_id } = video;
+    const filename = `${lesson_id.padStart(2, "0")}_${title.replace(
+      /\s+/g,
+      "_"
+    )}_${path.basename(url)}`;
+    const filePath = path.join(videosDir, filename);
+
+    try {
+      console.log(`Start downloading: ${filename}`);
+      const response = await axios({
+        method: "get",
+        url,
+        responseType: "stream",
+      });
+
+      await pipeline(response.data, fs.createWriteStream(filePath));
+      console.log(`Downloaded: ${filename}`);
+    } catch (error) {
+      console.error(`Error downloading ${filename}:`, error.message);
+    }
   }
-  const { title, url, lesson_id } = video;
-  const filename = `${lesson_id.padStart(2, "0")}_${title.replace(
-    /\s+/g,
-    "_"
-  )}_${path.basename(url)}`;
-  const filePath = path.join(videosDir, filename);
+}
 
-  try {
-    console.log(`Start downloading: ${filename}`);
-    const response = await axios({
-      method: "get",
-      url,
-      responseType: "stream",
-    });
+async function downloadTranscripts(transcripts, courseTitle, courseId) {
+  if (TRANSCRIPT_LANGUAGES.length < 1) {
+    console.warn("Configured not to download transcripts, skipping.");
+    return;
+  }
+  if (!transcripts || !Array.isArray(transcripts)) {
+    throw new Error("Invalid course downloads data");
+  }
 
-    await pipeline(response.data, fs.createWriteStream(filePath));
-    console.log(`Downloaded: ${filename}`);
-  } catch (error) {
-    console.error(`Error downloading ${filename}:`, error.message);
+  const wantedTranscripts = transcripts
+    .map((transcript) => ({
+      ...transcript,
+      downloads: transcript.downloads.filter(
+        (download) =>
+          TRANSCRIPT_LANGUAGES.some((language) =>
+            download.language.includes(language)
+          ) && download.url !== null
+      ),
+    }))
+    .filter((transcript) => transcript.downloads.length > 0);
+
+  if (wantedTranscripts.length < 1) {
+    console.warn("None of the transcript languages you configured were found.");
+    return;
+  }
+
+  console.log(
+    `Total ${wantedTranscripts.length} transcript file to download for course ${courseTitle}.`
+  );
+
+  const transcriptDir =
+    `downloads/${courseId}_${courseTitle}/transcripts`.replace(/[\s:]+/g, "_");
+
+  if (!fs.existsSync(transcriptDir)) {
+    fs.mkdirSync(transcriptDir, { recursive: true });
+  }
+
+  while (wantedTranscripts.length > 0) {
+    const transcript = wantedTranscripts.shift();
+    const { title, lesson_id: lessonId, downloads } = transcript;
+
+    while (downloads.length > 0) {
+      const download = downloads.shift();
+      const { language } = download;
+      let url = download.url;
+
+      //Unfortunately transcripts are in a GoogleDrive and the urls are not download friendly try to convert to download urls.
+      if (url.includes("drive.google.com")) {
+        const regex = /\/d\/([a-zA-Z0-9_-]+)/;
+        const match = url.match(regex);
+        const fileId = match ? match[1] : null;
+
+        if (!fileId) {
+          console.warn(`Could not find GoogleDrive file id for ${url} skipping`);
+          return;
+        }
+        url = `https://drive.usercontent.google.com/download?id=${fileId}&export=download&authuser=0`;
+      }
+
+      const filename = `${lessonId.padStart(2, "0")}_${title.replace(
+        /\s+/g,
+        "_"
+      )}_${language}.pdf`; //Guessing that all are pdfs since we dont get file extension when downloading from drive.
+      const filePath = path.join(transcriptDir, filename);
+      try {
+        console.log(`Start downloading: ${filename}`);
+        const response = await axios({
+          method: "get",
+          url,
+          responseType: "stream",
+        });
+
+        await pipeline(response.data, fs.createWriteStream(filePath));
+        console.log(`Downloaded: ${filename}`);
+      } catch (error) {
+        console.error(`Error downloading ${filename}:`, error.message);
+      }
+    }
   }
 }
 
@@ -108,23 +205,17 @@ async function fetchData(courseId) {
     const courseDownloadsUrl = `${baseDownloadsUrl}?id=${courseId}`;
     const response = await axios.get(courseDownloadsUrl);
     const {
-      course_downloads: courseDownloads,
+      course_downloads: videos,
+      transcripts,
+      audio,
+      books,
+      assignments,
       title: courseTitle,
       course_id: responseCourseId,
     } = response.data.data;
 
-    if (!courseDownloads || !Array.isArray(courseDownloads)) {
-      throw new Error("Invalid course downloads data");
-    }
-
-    const queue = [...courseDownloads];
-    console.log(
-      `Total ${queue.length} video files to download for course ${courseTitle}.`
-    );
-
-    while (queue.length > 0) {
-      await downloadVideo(queue.shift(), courseTitle, responseCourseId);
-    }
+    await downloadVideo(videos, courseTitle, responseCourseId);
+    await downloadTranscripts(transcripts, courseTitle, responseCourseId);
 
     console.log("All downloads completed!");
   } catch (error) {
